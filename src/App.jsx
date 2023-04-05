00a68344113303f94b66ad502573139b5a2f41d3
Carlos Vera <carlosvera.cv750@gmail.com>
2023-04-05 17:12:48 -0400

    First configuration of the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainContainer from './containers/MainContainer';
+import Header from './components/Header';
+
+function App() {
+
+  return (
+    <MainContainer>
+      <Header></Header>
+    </MainContainer>
+  );
+}
+
+export default App;
---
+++ src/containers/MainContainer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MainContainer.css';
+
+const MainContainer = ({ children }) => {
+  return(
+    <main className="MainContainer">
+      {children}
+    </main>
+  )
+};
+
+export default MainContainer;
